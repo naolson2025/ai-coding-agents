92d20d624fdc7a78f8814214ea33c2138304908b
Nick Olson <45021396+naolson2025@users.noreply.github.com>
2025-10-03 21:48:04 -0500

    added jules setup script

---
+++ jules-setup.ts
@@ -0,0 +1,14 @@
+// @ts-nocheck
+
+import { $ } from 'bun'
+
+await $`bun install`
+await $`cp .env.example .env`
+await $`sudo apt install -y postgresql`
+await $`sudo service postgresql start`
+await $`sudo -u postgres psql -c "CREATE ROLE \"user\" WITH LOGIN PASSWORD 'password';"`
+await $`sudo -u postgres psql -c "CREATE DATABASE todos OWNER \"user\";"`
+await $`sudo -u postgres psql -c "ALTER ROLE \"user\" WITH CREATEDB;"`
+await $`bun run db:migrate`
+await $`bun run db:seed`
+await $`bun run test`
